19b9d303df7b7ad57ed8fe782f6516975bd811f7
Julia <julialopezcalvo95@gmail.com>
2025-03-20 20:05:49 +0100

    datos als ervidor

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-const express = require('express');
-const cors = require('cors');
 const mysql = require ('mysql2/promise');
 
-// create and config server
-const server = express();
-server.use(cors());
-server.use(express.json());
+const express = require('express');
+const cors = require('cors');
 
-// init express aplication
-const serverPort = 4000;
-server.listen(serverPort, () => {
-    console.log(`Server listening at http://localhost:${serverPort}`);
+
+async function getConnection() {
+    const connectionData = {
+        host: process.env["MYSQL_HOST"],
+        port: process.env["MYSQL_PORT"],
+        user: process.env["MYSQL_USER"],
+        password: process.env["MYSQL_PASS"],
+        database: process.env["MYSQL_SCHEMA"]
+
+    };
+    const connection = await mysql.createConnection(connectionData);
+    await connection.connect();
+    return connection;
+}
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//Arrancamos Servidor
+
+const port = 14573;
+app.listen(port, () => {
+    console.log(`Example app listening on port<http://localhost:${port}>`);
 });
- 
