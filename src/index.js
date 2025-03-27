dfd53b13ceeddcbee7dcfa968b2a5fd9e8b36c11
Julia <julialopezcalvo95@gmail.com>
2025-03-27 11:21:27 +0100

    projectdata

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ app.use(express.json({ limit: '50mb' }));
 // Ruta de ejemplo
 app.get('/', (req, res) => {
     const idUnico = uuid(); // Generar un UUID único
-    res.render('projectCardDetail', { id: idUnico }); // Pasar el UUID a la plantilla
+    res.render('projectCardDetail',{projectData}, { id: idUnico }); // Pasar el UUID a la plantilla
   });
 
 
